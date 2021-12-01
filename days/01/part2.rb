f1aafd220bd1a7756d2b5c745ba3027af3fdc311
Anna <anna328p@gmail.com>
2021-11-30 23:13:47 -0600

    ruby: day 1

--- days/01/part2.rb
+++ days/01/part2.rb
@@ -4,10 +4,10 @@
 
 # Advent of Code 2021
 # Day 1
-# Part 1
+# Part 2
 
 infile = ARGV[0] || 'input.txt'
 
 input = File.readlines(infile).map(&:to_i)
 
-p input.each_cons(2).count { |a, b| b > a }
+p input.each_cons(4).count { |a, b, c, d| b + c + d > a + b + c }
