a57eebba1d968c1d3cdf937a8a0d3cfea581791d
Anna <anna@hermes.ad.ap5.dev>
2021-12-15 00:19:38 -0600

    speed up a* algorithm with priority queue

--- lib/grids/pathfinding.rb
+++ lib/grids/pathfinding.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
-require 'set'
+require 'pqueue'
 
 # A* algorithm
 module Grids
   # @private
-  # @param came_from [Hash<Node, Node>] the came_from hash
-  # @param current [Node] the current node
-  # @return [Array<Node>] the path
+  # @param came_from [Hash<Node, Node>] came_from hash
+  # @param current [Node] current node
+  # @return [Array<Node>] reconstructed path
   def self.reconstruct_path(came_from, current)
     total_path = [current]
     while came_from.key?(current)
@@ -19,16 +19,15 @@ module Grids
 
   # A* search.
   # @param nodes [AbstractNodeSet] a set of nodes
-  # @param start [Node] the start node
-  # @param goal [Node] the goal node
+  # @param start [Node] start node
+  # @param goal [Node] goal node
   # @yield [start, goal] heuristic function
-  # @yieldparam start [Node] the start node
-  # @yieldparam goal [Node] the goal node
+  # @yieldparam start [Node] heuristic start node
+  # @yieldparam goal [Node] goal node
   # @return [Array<Node>] the path from start to goal
   # rubocop: disable Metrics/MethodLength
   # rubocop: disable Metrics/AbcSize
   def self.a_star(nodes, start, goal, &heuristic)
-    open_set = Set.new([start])
     came_from = {}
 
     g_score = {}
@@ -37,12 +36,12 @@ module Grids
     f_score = {}
     f_score[start] = heuristic.call(start, goal)
 
+    open_set = PQueue.new([start]) { |a, b| f_score[a] < f_score[b] }
+
     until open_set.empty?
-      current = open_set.min_by { f_score[_1] }
+      current = open_set.pop
 
       return reconstruct_path(came_from, current) if current == goal
-
-      open_set.delete(current)
 
       nodes.neighbors(current).each do |neighbor|
         tentative_g_score = g_score[current] + nodes.weight(current, neighbor)
@@ -52,7 +51,8 @@ module Grids
         came_from[neighbor] = current
         g_score[neighbor] = tentative_g_score
         f_score[neighbor] = g_score[neighbor] + heuristic.call(neighbor, goal)
-        open_set.add(neighbor)
+
+        open_set << neighbor
       end
     end
 
